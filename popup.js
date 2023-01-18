aa0ad08004515d95ff265e60dd640e392f93cb56
Grégory Clain <g.clain69@gmail.com>
2023-01-18 23:50:35 +0100

    add copy to clipboard

--- popup.js
+++ popup.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let generateNameButton = document.getElementById('generateName');
-    let displayNameZone = document.getElementById('displayName');
+    const generateNameButton = document.getElementById('generateName');
+    const displayNameZone = document.getElementById('displayName');
+    const copyNameButton = document.getElementById('copyName');
     generateNameButton.addEventListener('click', function() {
-        console.log('test console')
         displayNameZone.value = "Test text"
+    })
+    copyNameButton.addEventListener('click', async function() {
+        try {
+            await navigator.clipboard.writeText(displayNameZone.value);
+            console.log("Texte copié dans le presse-papiers.")
+        } catch (err) {
+            console.error("Impossible de copier le texte : ", err);
+        }
     })
 })
 
