1754d72fc07bebf22f5f7ca3410b96f658ba125e
Grégory Clain <gregory.clain@ext.euronews.com>
2023-01-19 16:33:37 +0100

    copy ok

--- popup.js
+++ popup.js
@@ -18,42 +18,70 @@ async function getTabID() {
     })
 }
 
-function generateName(htmlName){
-    console.log('htmlName', htmlName)
+function generateName(stringName){
+    console.log('htmlName', stringName)
+    const idPR = stringName.match(/\(([^)]+)\)/);
+    const scopePR = stringName.match(/\[([^\]]+)\]/);
+    const msgPR = stringName.match(/\](.*)$/);
+    const tagPR = document.getElementById('tagName').value ? document.getElementById('tagName').value : "6.X.X" ;
+
+    const stringToPost = ':euronews: MEP ' + tagPR + ' ' + idPR[1] + scopePR[0] + ': ' + msgPR[1] ;
+    return stringToPost;
 }
 
+// async function chromeScripting(){
+//     const tabId = await getTabID();
+//     console.log('tabId',tabId )
+//     let result;
+//     return chrome.scripting.executeScript({
+//         target: {tabId: tabId, allFrames: true},
+//             func: getH1,
+//         },
+//         (injectionResults) => {
+//             result = injectionResults[0].result;
+//             return injectionResults[0].result;
+//             // generateName(injectionResults[0].result);
+//         });
 
+// }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const generateNameButton = document.getElementById('generateName');
     const displayNameZone = document.getElementById('displayName');
-    const copyNameButton = document.getElementById('copyName');
+    const generateNameButton = document.getElementById('generateButton');
+    const copyNameButton = document.getElementById('copyButton');
+    const eraseNameButton = document.getElementById('eraseButton');
+    const tagNameButton = document.getElementById('tagName')
+    
 
-    generateNameButton.addEventListener('click', function() {
-        displayNameZone.value = ":euronews: MEP 6.X.X EW-XXXX[XX]: Message"
-    })
-
-
-    copyNameButton.addEventListener('click', async function() {
+    generateNameButton.addEventListener('click', async function() {
         try {
-
-            // fonction copy to clipboard
-            await navigator.clipboard.writeText(displayNameZone.value);
-            console.log("Texte copié dans le presse-papiers.")
-            
             // récupérer les infos de la tab courante
             const tabId = await getTabID();
+            // const test1 = await chromeScripting();
             await chrome.scripting.executeScript(
                 {
                   target: {tabId: tabId, allFrames: true},
                   func: getH1,
                 },
                 (injectionResults) => {
-                    generateName(injectionResults[0].result);
+                    displayNameZone.value = generateName(injectionResults[0].result);
                 });
+        } catch (err) {
+            console.error(err);
+        }
+    })
+
+    copyNameButton.addEventListener('click', async function() {
+        try { 
+            await navigator.clipboard.writeText(displayNameZone.value);
+            console.log("Texte copié dans le presse-papiers.")
         } catch (err) {
             console.error("Impossible de copier le texte : ", err);
         }
+    })
+    eraseNameButton.addEventListener('click', async function() {
+        displayNameZone.value ="";
+        tagNameButton.value ="";
     })
 })
 
